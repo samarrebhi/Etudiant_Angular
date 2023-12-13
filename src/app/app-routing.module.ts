58d25f4556f23d7f17408a2f2b8a581efd7c72e2
samar <samar.rebhi@esprit.tn>
2023-12-13 05:12:02 +0100

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
 
 const routes: Routes = [
